0ee37ec10bcb4b70014d694d542b372020282e21
Jose Daniel Hernandez <jd@nimiq.com>
2022-06-28 00:41:20 -0600

    Update the `nimiq-client` dependency
    Update the `nimiq-client` dependency and change the script to work
    properly with the new version.
    Also clean up dependencies that are no longer needed.

--- pool-payouts.py
+++ pool-payouts.py
@@ -1,13 +1,12 @@
 import argparse
 import asyncio
-import json
-from jsonrpcclient import Ok, parse_json, request_json
 import logging
 from nimiqclient import *
-import websockets
 
 LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 DEFAULT_LOG_LEVEL = "INFO"
+
+pool_fee = 0
 
 
 class Range(object):
@@ -19,20 +18,20 @@ class Range(object):
         return self.start <= other <= self.end
 
 
-async def process_block(client, hash, pool_fee):
+async def process_block(client, hash):
     logging.debug("Block hash: {}".format(hash))
-    block = client.get_block_by_hash(hash, False)
+    block = await client.get_block_by_hash(hash, False)
     # Nothing to do when the block is a micro block
     if block.type == "micro":
         return
     logging.info("Running for block {}".format(block.number))
-    validator_address = client.get_validator_address()
-    validator = client.get_validator_by_address(
+    validator_address = await client.get_validator_address()
+    validator = await client.get_validator_by_address(
         validator_address, include_stakers=True)
-    reward_account = client.get_account_by_address(validator.rewardAddress)
+    reward_account = await client.get_account_by_address(
+        validator.rewardAddress)
     total_staked_balance = 0
-    # steak_porc = []
-    # validator_balance = validator.balance
+
     for staker in validator.stakers:
         total_staked_balance += staker.balance
     for staker in validator.stakers:
@@ -41,11 +40,11 @@ async def process_block(client, hash, pool_fee):
             float(reward_account.balance) * (1.0 - pool_fee))
         sender = reward_account.address
         recipient = staker.address
-        if client.is_account_unlocked(reward_account.address):
+        if await client.is_account_unlocked(reward_account.address):
             if sender != recipient:
                 logging.info("Sending reward of {} to address {}".format(
                     amount_to_send, recipient))
-                client.send_basic_transaction(
+                await client.send_basic_transaction(
                     sender, recipient, amount_to_send, 0, block.number)
         else:
             raise InternalErrorException(
@@ -54,31 +53,38 @@ async def process_block(client, hash, pool_fee):
             )
 
 
-async def run_client(uri, client, pool_fee):
-    async with websockets.connect(uri) as ws:
-        # call concat on the other side
-        call_object = {
-            "jsonrpc": "2.0",
-            "method": "headSubscribe",
-            "params": [],
-            "id": 0,
-        }
-        await ws.send(json.dumps(call_object))
-        # response = parse_json(await ws.recv())
-        # print result
-        # if isinstance(response, Ok):
-        #    print(response)
-        # else:
-        #    print(response.message)
-        result = parse_json(await ws.recv())
-        subscription_id = result.result
-        async for message in ws:
-            # print(message)
-            message = json.loads(message)
-            if 'params' in message:
-                if message['params']['subscription'] == subscription_id:
-                    await process_block(client, message['params']['result'],
-                                        pool_fee)
+async def run_client(host, port, private_key):
+    async with NimiqClient(
+        scheme="ws", host=host, port=port
+    ) as client:
+        try:
+            # Get consensus
+            consensus = await client.consensus()
+            logging.info("Consensus: {0}".format(consensus))
+        except InternalErrorException as error:
+            logging.critical(
+                "Got error when trying to connect to the RPC server: {0}"
+                .format(str(error)))
+
+        # Import reward account private key
+        await client.importRawKey(private_key)
+        # Get the validator this is running for
+        validator_address = await client.get_validator_address()
+        validator = await client.get_validator_by_address(
+            validator_address, include_stakers=False)
+        reward_account = await client.get_account_by_address(
+            validator.rewardAddress)
+        await client.unlock_account(reward_account.address)
+        if not await client.is_account_unlocked(reward_account.address):
+            raise InternalErrorException(
+                "Couldn't unlocked Validator reward address ({0}),"
+                "perhaps an incorrect private key was specified?".format(
+                    reward_account.address)
+            )
+        if consensus:
+            await client.head_subscribe(process_block)
+            while True:
+                await asyncio.sleep(1)
 
 
 def parse_args():
@@ -135,40 +141,13 @@ def main():
     # Setup logging
     setup_logging(args)
 
-    # Create Nimiq RPC client
-    client = NimiqClient(
-        scheme="http", host=args.host, port=args.port
+    # Setup the pool fee global variable
+    global pool_fee
+    pool_fee = args.pool_fee
+
+    asyncio.get_event_loop().run_until_complete(
+        run_client(args.host, args.port, args.private_key)
     )
-
-    try:
-        # Get consensus
-        consensus = client.consensus()
-        logging.info("Consensus: {0}".format(consensus))
-    except InternalErrorException as error:
-        logging.critical(
-            "Got error when trying to connect to the RPC server: {0}".format(
-                str(error)))
-
-    # Import reward account private key
-    client.importRawKey(args.private_key)
-    # Get the validator this is running for
-    validator_address = client.get_validator_address()
-    validator = client.get_validator_by_address(
-        validator_address, include_stakers=False)
-    reward_account = client.get_account_by_address(validator.rewardAddress)
-    client.unlock_account(reward_account.address)
-    if not client.is_account_unlocked(reward_account.address):
-        raise InternalErrorException(
-            "Couldn't unlocked Validator reward address ({0}),"
-            "perhaps an incorrect private key was specified?".format(
-                reward_account.address)
-        )
-    if consensus:
-        # client.head_subscribe(process_block)
-        asyncio.get_event_loop().run_until_complete(
-            run_client("ws://{}:{}/ws".format(args.host,
-                       args.port), client, args.pool_fee)
-        )
 
 
 if __name__ == "__main__":
